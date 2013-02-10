5d34f2e0a6f3f814e33efb557417197119983618
TJ Vanderpoel <tj@rubyists.com>
2013-02-10 11:22:29 -0600

    make sure everything is attributed

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,13 @@
-default['lighttpd2']['prefix'] = '/opt/lighttpd2'
-default['lighttpd2']['etc'] = '/opt/lighttpd2/etc'
-default['lighttpd2']['conf_dir'] = '/opt/lighttpd2/etc/conf.d'
+default['lighttpd2']['src_prefix'] = '/usr/local/src' # Where to download the sources (+ /lighttpd2)
+default['lighttpd2']['build_prefix'] = '/usr/local/build' # Where to build the sources (+ /lighttpd2)
+default['lighttpd2']['prefix'] = '/opt/lighttpd2' # Install prefix
+default['lighttpd2']['etc'] = '/opt/lighttpd2/etc' # Etc directory
+default['lighttpd2']['conf_dir'] = '/opt/lighttpd2/etc/conf.d' # Where additional configuration files go
 default['lighttpd2']['listen_addresses'] = ['0.0.0.0:80']
 default['lighttpd2']['openssl']['enable'] = true
 default['lighttpd2']['openssl']['listen_addresses'] = ['0.0.0.0:443']
-default['lighttpd2']['modules'] = [:accesslog, :openssl, :lua, :proxy, :balance, :expire, :vhost]
+default['lighttpd2']['modules'] = [:accesslog, :openssl, :lua, :proxy, :balance, :expire, :vhost, :redirect]
+default['lighttpd2']['index_files'] = %w{index.html index.html}
 default['lighttpd2']['applications'] = []
 default['lighttpd2']['run_as'] = "www-data"
+default['lighttpd2']['git_reference'] = "master"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,14 @@
   end
 end
 
-directory "/usr/local/build" do
+directory "#{node['lighttpd2']['build_prefix']}" do
   action :create
 end
 
 bash "compile_lighttpd2" do
-  cwd "/usr/local/build/lighttpd2"  
+  cwd "#{node['lighttpd2']['build_prefix']}/lighttpd2"  
   code <<-EOF
+    [ -d #{node['lighttpd2']['prefix']}/lighttpd2 ] && mv #{node['lighttpd2']['prefix']}/lighttpd2 #{node['lighttpd2']['prefix']}/lighttpd2.#{Time.to_i}
     ./autogen.sh
     ./configure --with-lua --with-openssl --prefix=#{node['lighttpd2']['prefix']}
     make
@@ -19,20 +20,20 @@ bash "compile_lighttpd2" do
   action :nothing
 end
 
-bash "clone_lighttpd2" do
-  cwd "/usr/local/build"
+bash "copy_lighttpd2" do
+  cwd "#{node['lighttpd2']['build_prefix']}"
   code <<-EOF
-    [ -d ./lighttpd2 ] && rm -rf ./lighttpd2
-    git clone /usr/local/src/lighttpd2
+    [ -d ./lighttpd2 ] && rm -rf ./lighttpd2 ]
+    cp -a #{node['lighttpd2']['src_prefix']}/lighttpd2 ./
   EOF
   action :nothing
 end
 
-git "/usr/local/src/lighttpd2" do
+git "#{node['lighttpd2']['src_prefix']}/lighttpd2" do
   repository "git://git.lighttpd.net/lighttpd/lighttpd2.git"
-  reference "master"
+  reference "#{node['lighttpd2']['git_reference']}"
   action :sync
-  notifies :run, "bash[clone_lighttpd2]", :immediately
+  notifies :run, "bash[copy_lighttpd2]", :immediately
   notifies :run, "bash[compile_lighttpd2]", :immediately
 end
 
@@ -58,7 +59,7 @@ template "#{node['lighttpd2']['etc']}/angel.conf" do
 end
 
 execute "copy_mimetypes" do
-  command "cp /usr/local/build/lighttpd2/doc/mimetypes.conf #{node['lighttpd2']['etc']}/ && chown #{node['lighttpd2']['run_as']} #{node['lighttpd2']['etc']}/mimetypes.conf"
+  command "cp #{node['lighttpd2']['build_prefix']}/lighttpd2/doc/mimetypes.conf #{node['lighttpd2']['etc']}/ && chown #{node['lighttpd2']['run_as']} #{node['lighttpd2']['etc']}/mimetypes.conf"
   creates "#{node['lighttpd2']['etc']}/mimetypes.conf"
 end
 
@@ -67,34 +68,98 @@ execute "make_fake_cert" do
   creates "#{node['lighttpd2']['etc']}/server.pem"
 end
 
-directory "/etc/sv/lighttpd2/log" do
+directory "#{node['runit']['sv_dir']}/lighttpd2/log" do
   recursive true
   action :create
 end
 
-template "/etc/sv/lighttpd2/run" do
+template "#{node['runit']['sv_dir']}/lighttpd2/run" do
   source "lighttpd.run.erb"
   mode 0700
 end
 
-link "/etc/sv/lighttpd2/log/run" do
-  to "/usr/local/bin/rsvlog"
+if File.exists?("/usr/local/bin/rsvlog")
+  link "#{node['runit']['sv_dir']}/lighttpd2/log/run" do
+    to "/usr/local/bin/rsvlog"
+  end
+elsif File.exists?("/usr/bin/rsvlog")
+  link "#{node['runit']['sv_dir']}/lighttpd2/log/run" do
+    to "/usr/bin/rsvlog"
+  end
+else
+  file("#{node['runit']['sv_dir']}/lighttpd2/log/run") do
+    content <<-EOF
+#!/usr/bin/env bash 
+set -e
+if [ $0 != "./run" ];then
+  echo "This script meant to be linked as ./run in a service/log directory only!"
+  exit 1
+fi
+curdir=$(basename $(pwd))
+if [ "$curdir" != "log" ];then
+  echo "This script meant to be run from a service/log directory only!"
+  exit 1
+fi
+if [ -f ./conf ];then
+  source ./conf
+fi
+if [ ! -v SV_TIMESTAMP ];then
+  echo "Setting default timestamp"
+  # Default to tai64 timestamps
+  SV_TIMESTAMP="-t"
+fi
+if [ "x$SV_LOGDIR" != "x" ];then
+  logdir=$SV_LOGDIR
+fi
+if [ -w /var/log ];then
+  user_group=${USERGROUP:-daemon:adm}
+  if [ "x$logdir" == "x" ];then
+    logdir=$(basename $(dirname $(pwd)))
+  fi
+  [ -d "/var/log/$logdir" ] || mkdir -p "/var/log/$logdir"
+  [ -L ./main ] || [ -d ./main ] || ln -s "/var/log/$logdir" ./main
+  [ -L ./current ] || ln -s main/current
+  if [ "x$CURRENT_LOG_FILE" != "x" ];then
+    [ -L "/var/log/$logdir/$CURRENT_LOG_FILE" ] || ln -s current "/var/log/$logdir/$CURRENT_LOG_FILE"
+  fi
+  usergroup=$(stat -c "%U:%G" "/var/log/$logdir")
+  if [ "$usergroup" != "$user_group" ];then
+    chown -R $user_group "/var/log/$logdir"
+  fi
+  echo Logging as $user_group to /var/log/$logdir
+  exec chpst -u $user_group svlogd ${SV_TIMESTAMP} ./main
+else
+  echo Logging in $PWD
+  if [ "x$CURRENT_LOG_FILE" != "x" ];then
+    [ -L "$CURRENT_LOG_FILE" ] || ln -s current "$CURRENT_LOG_FILE"
+  fi
+  exec svlogd ${SV_TIMESTAMP} ./
+fi
+    EOF
+    mode 0750
+  end
 end
 
-link "/etc/sv/lighttpd2/supervise" do
+link "#{node['runit']['sv_dir']}/lighttpd2/supervise" do
   to "/var/run/lighttpd2.sv"
 end
 
-link "/etc/sv/lighttpd2/log/supervise" do
+link "#{node['runit']['sv_dir']}/lighttpd2/log/supervise" do
   to "/var/run/lighttpd2.log.sv"
 end
 
-link "/etc/service/lighttpd2" do
-  to "/etc/sv/lighttpd2"
+link "#{node['runit']['service_dir']}/lighttpd2" do
+  to "#{node['runit']['sv_dir']}/lighttpd2"
 end
 
 execute "restart_lighttpd2" do
   action :nothing
   command "sv t lighttpd2"
-  only_if { File.exists?("/etc/sv/lighttpd2/supervise/ok") }
+  only_if { File.exists?("#{node['runit']['service_dir']}/lighttpd2/supervise/ok") }
+end
+
+execute "restart_lighttpd2_log" do
+  action :nothing
+  command "sv t #{node['runit']['service_dir']}/lighttpd2/log"
+  only_if { File.exists?("#{node['runit']['service_dir']}/lighttpd2/log/supervise/ok") }
 end
